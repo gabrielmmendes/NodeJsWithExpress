2911f6db0ff46815ab188bb26cb7a163e66b47e5
Gabriel Mendes <gabrielmendes@labtime.ufg.br>
2022-08-08 20:01:08 -0300

    adição de novas rotas e do nodemon

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const server = express()
+server.use(express.json())
+const users = [ ]
 
-server.get('/geeks', () => {
-  console.log('teste')
+//req ➔ representa todos os dados da requisição que o cliente faz
+//res ➔ todas as informações necessárias para informar uma resposta para o front-end.
+
+server.get('/users', (req, res) => {
+  return res.json(users)
+})
+
+server.get('/users/:index', (req, res) => {
+  return res.json(req.user)
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body
+  users.push(name)
+  return res.json(users)
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+
+  users[index] = name
+  return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params
+
+  users.splice(index, 1)
+
+  return res.json(users)
 })
 
 server.listen(3000)
