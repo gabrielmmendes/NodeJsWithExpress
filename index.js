7b1762771de1033023bf3ae55c9aeb7450748897
Gabriel Mendes <gabrielmendes@labtime.ufg.br>
2022-08-20 19:48:25 -0300

    implementação do metodo de busca de usuario por codigo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const server = express()
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient, Prisma } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
@@ -13,7 +13,7 @@ server.listen(3000)
 
 server.get('/users', async (req, res) => {
   const currentPage = req.query.page || 0
-  const listPerPage = 5
+  const listPerPage = 10
   const offset = currentPage * listPerPage
 
   const users = await prisma.user.findMany({
@@ -21,24 +21,34 @@ server.get('/users', async (req, res) => {
     take: listPerPage
   })
 
-  users.forEach(user => {
-    user.codigo = Number(user.codigo.toString())
-  })
-
-  return res.json({
+  res.json({
     data: users,
     meta: { page: currentPage }
   })
 })
 
-server.get('/users/:index', (req, res) => {
-  return res.json(req.user)
+server.get('/users/:index', async (req, res) => {
+  const { index } = req.params
+
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {codigo: Number(index)},
+    })
+    res.json(user)
+  } catch (e) {
+    if (e instanceof Prisma.NotFoundError) {
+      res.json({erro: `Usuário com código ${index} não foi encontrado.`})
+    } else {
+      console.error(e.stack);
+      res.sendStatus(500).send('Internal Server Error')
+    }
+  }
 })
 
 server.post('/users', (req, res) => {
   const { name } = req.body
   users.push(name)
-  return res.json(users)
+  res.json(users)
 })
 
 server.put('/users/:index', (req, res) => {
@@ -46,7 +56,7 @@ server.put('/users/:index', (req, res) => {
   const { name } = req.body
 
   users[index] = name
-  return res.json(users)
+  res.json(users)
 })
 
 server.delete('/users/:index', (req, res) => {
@@ -54,5 +64,5 @@ server.delete('/users/:index', (req, res) => {
 
   users.splice(index, 1)
 
-  return res.json(users)
+  res.json(users)
 })
